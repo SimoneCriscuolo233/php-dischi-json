5462393f42e6e84bd7fd90b9a75ebc6fd2464052
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-12-15 15:39:19 +0100

    feat: Add form to allow users to add new disk entries

--- index.php
+++ index.php
@@ -20,6 +20,44 @@ $disks = readDisks();
 <div class="container py-5">
 
     <h1 class="text-center mb-5">Music Disks</h1>
+    <div class="card mb-5">
+    <div class="card-body">
+        <h5 class="card-title">Add new disk</h5>
+
+        <form action="server.php" method="POST" class="row g-3">
+            <div class="col-md-6">
+                <input class="form-control" name="title" placeholder="Title">
+            </div>
+
+            <div class="col-md-6">
+                <input class="form-control" name="artist" placeholder="Artist">
+            </div>
+
+            <div class="col-md-6">
+                <input class="form-control" name="cover" placeholder="Cover URL">
+            </div>
+
+            <div class="col-md-3">
+                <input class="form-control" type="number" name="year" placeholder="Year">
+            </div>
+
+            <div class="col-md-3">
+                <input class="form-control" name="genre" placeholder="Genre">
+            </div>
+
+            <div class="col-12">
+                <button class="btn btn-primary">Add Disk</button>
+            </div>
+        </form>
+    </div>
+</div>
+    <!-- {
+        "title": "The Dark Side of the Moon",
+        "artist": "Pink Floyd",
+        "cover": "https:\/\/picsum.photos\/400\/300?random=3",
+        "year": "1973",
+        "genre": "Progressive Rock"
+    } -->
 
     <div class="row g-4">
         <?php foreach ($disks as $disk): ?>
